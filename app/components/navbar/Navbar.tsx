f595ef42ed7dce6d0ac3e273a96d5e1a118099ee
kenjaminbutton <kennethpchang@gmail.com>
2023-07-03 17:06:12 +0800

    Navbar UI flushed out

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,10 +1,12 @@
 import Container from "../Container"
 import Logo from './Logo'
+import Search from "./Search"
+import UserMenu from "./UserMenu"
+
 
 const Navbar = () => {
   return (
     <div className='fixed w-full bg-white z-10 shadow-sm'>
-      I am your cute Navbar
       <div className='py-4 border-b-[1px]'>
         <Container>
           <div className='
@@ -16,6 +18,8 @@ const Navbar = () => {
             md:gap-0
           '>
             <Logo />
+            <Search />
+            <UserMenu />
           </div>
 
         </Container>
