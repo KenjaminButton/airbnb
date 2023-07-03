520324b049205d9603b0afdc24dba0e6de80a413
kenjaminbutton <kennethpchang@gmail.com>
2023-07-03 17:28:20 +0800

    UI finished for navigation

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -1,10 +1,14 @@
 'use client';
 import { AiOutlineMenu } from 'react-icons/ai'
 import Avatar from '../Avatar';
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
+import MenuItem from './MenuItem';
 
 const UserMenu = () => {
   const [isOpen, setIsOpen] = useState(false)
+  const toggleOpen = useCallback(() => {
+    setIsOpen((value) => !value)
+  }, [])
 
   return (
     <div className='relative'>
@@ -27,7 +31,7 @@ const UserMenu = () => {
           Airbnb your home
         </div>
         <div
-          onClick={() => { }}
+          onClick={toggleOpen}
           className='
             p-4
             md:py-1
@@ -50,7 +54,36 @@ const UserMenu = () => {
           </div>
         </div>
       </div>
-    </div>
+      {isOpen && (
+        <div
+          className='
+            absolute
+            rounded-xl
+            shadow-md
+            w-[40vw]
+            md:w-3/4
+            bg-white
+            overflow-hidden
+            right-0
+            top-12
+            text-sm
+          '
+        >
+          <div className='flex flex-col cursor-pointer'>
+            <>
+              <MenuItem
+                onClick={() => { }}
+                label='Login'
+              />
+              <MenuItem
+                onClick={() => { }}
+                label='Sign Up'
+              />
+            </>
+          </div>
+        </div>
+      )}
+    </div >
   )
 }
 
